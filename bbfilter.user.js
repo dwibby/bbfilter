36d92d4feef23ab9fb2feac9d6eac53e113d9163
dwibby <36788397+dwibby@users.noreply.github.com>
2020-05-19 10:34:29 -0400

    Create bbfilter.user.js

---
+++ bbfilter.user.js
@@ -0,0 +1,15 @@
+ // ==UserScript==
+// @name         bbfilter
+// @namespace    https://github.com/dwibby/
+// @version      0.1
+// @description  Filters bb articles
+// @author       dwibby
+// @match        *://boingboing.net/blog
+// @grant        none
+// @run-at       document-idle
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// ==/UserScript==
+
+$(document).ready(function(){
+  $("a[rel*='author']:contains('Boing Boing\\'s Shop')").parents(".byline").parents("article").hide();
+});
